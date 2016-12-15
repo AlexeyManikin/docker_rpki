ddf1317ea6751f2a890df1161c8b839986952cb5
/usr/bin/vim <alexey@beget.ru>
2016-12-16 00:27:42 +0300

    new version

--- rpki/startup.sh
+++ rpki/startup.sh
@@ -8,6 +8,6 @@ chmod 644 /etc/resolv.conf /etc/hosts* /etc/localtime || true
 chmod 711 / /etc /var/log || true
 chmod 777 /tmp -R || true
 
-cd /home/rpki-validator-app-2.22
+cd /home/rpki-validator-app-2.23
 ./rpki-validator.sh start  -c /home/my-configuration.conf
 
